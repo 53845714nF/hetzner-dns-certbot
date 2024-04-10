1445dfe686a5486fe057cef5734619e755838ddc
53845714nF <seba98feustel@gmail.com>
2024-04-11 00:16:02 +0200

    clean script

--- certbot-hetzner-auth.sh
+++ certbot-hetzner-auth.sh
@@ -1,17 +1,33 @@
-#!/bin/bash
+#!/bin/env bash
 
-token=$(cat /etc/hetzner-dns-token)
-search_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
+if [ -n "$TOKEN" ]; then
+  token=$TOKEN
+else
+  token=$(cat /etc/hetzner-dns-token)
+fi
 
-zone_id=$(curl \
+search_name=$( echo "$CERTBOT_DOMAIN" | rev | cut -d'.' -f 1,2 | rev)
+
+zone_id=$(curl -Ss\
         -H "Auth-API-Token: ${token}" \
         "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
         jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
 
-curl -X "POST" "https://dns.hetzner.com/api/v1/records" \
+data=$(cat <<EOF
+{
+  "value": "${CERTBOT_VALIDATION}",
+  "ttl": 300,
+  "type": "TXT",
+  "name": "_acme-challenge.${CERTBOT_DOMAIN}.",
+  "zone_id": "${zone_id}"
+}
+EOF
+)
+
+curl -Ss -X "POST" "https://dns.hetzner.com/api/v1/records" \
      -H 'Content-Type: application/json' \
      -H "Auth-API-Token: ${token}" \
-     -d "{ \"value\": \"${CERTBOT_VALIDATION}\", \"ttl\": 300, \"type\": \"TXT\", \"name\": \"_acme-challenge.${CERTBOT_DOMAIN}.\", \"zone_id\": \"${zone_id}\" }" > /dev/null 2>/dev/null
+     -d "$data"
 
 # just make sure we sleep for a while (this should be a dig poll loop)
 sleep 30
--- certbot-hetzner-cleanup.sh
+++ certbot-hetzner-cleanup.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
+#!/bin/env bash
 
-token=$(cat /etc/hetzner-dns-token)
-search_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
+if [ -n "$TOKEN" ]; then
+  # To use this run 'export $TOKEN=foo'before runing certbot
+  token=$TOKEN
+else
+  token=$(cat /etc/hetzner-dns-token)
+fi
 
-zone_id=$(curl \
+search_name=$( echo "$CERTBOT_DOMAIN" | rev | cut -d'.' -f 1,2 | rev)
+
+zone_id=$(curl -Ss \
         -H "Auth-API-Token: ${token}" \
         "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
         jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
 
-record_ids=$(curl \
+record_ids=$(curl -Ss \
         -H "Auth-API-Token: $token" \
         "https://dns.hetzner.com/api/v1/records?zone_id=$zone_id" | \
-       jq ".\"records\"[] | select(.name == \"_acme-challenge.${CERTBOT_DOMAIN}.\") | .id" 2>/dev/null | tr -d '"')
+        jq ".\"records\"[] | select(.name == \"_acme-challenge.${CERTBOT_DOMAIN}.\") | .id" 2>/dev/null | tr -d '"')
 
 for record_id in $record_ids
 do
-        curl -H "Auth-API-Token: $token" \
-                -X "DELETE" "https://dns.hetzner.com/api/v1/records/${record_id}" > /dev/null 2> /dev/null
+        curl -Ss \
+        -H "Auth-API-Token: $token" \
+        -X "DELETE" "https://dns.hetzner.com/api/v1/records/${record_id}" > /dev/null 2> /dev/null
 done
